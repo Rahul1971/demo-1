7f7ffa706791dc656b195279fcb27322239da209
Rahul1971 <rahulgandham1997@gmail.com>
2025-02-15 19:44:48 +0530

    Adding 1st line

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 echo This is to test the output
-echo This is to test the second output &>> /tmp/test.log
-echo This is to test the third output &>> /tmp/test.log
-echo This is to test the fourth output &>> /tmp/test.log
+echo This is to test the second output
+echo This is to test the third output &>>/tmp/test.log
+echo This is to test the fourth output &>>/tmp/test.log
 
 
