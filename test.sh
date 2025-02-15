be2e9f0e5c901f3ca486b0b733096f958f3c8619
Rahul1971 <rahulgandham1997@gmail.com>
2025-02-15 20:02:24 +0530

    Adding 1st line

--- test.sh
+++ test.sh
@@ -3,4 +3,6 @@ echo -e "\e[31mThis is to test the second output\e[0m"
 echo -e "\e[32mThis is to test the third output\e[0m"
 echo -e "\e[33mThis is to test the fourth output\e[0m"  &>>/tmp/test.log
 echo -e "\e[34mThis is to test the fourth output\e[0m"
+echo -e "\e[35mThis is to test the fifth output\e[0m"
+
 
