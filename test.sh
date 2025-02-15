85455014b3786fa2752d0981ac258fb8031276be
Rahul1971 <rahulgandham1997@gmail.com>
2025-02-15 20:10:08 +0530

    Adding 1st line

--- test.sh
+++ test.sh
@@ -5,13 +5,18 @@ echo -e "\e[33mThis is to test the fourth output\e[0m"  &>>/tmp/test.log
 echo -e "\e[34mThis is to test the fourth output\e[0m"
 echo -e "\e[35mThis is to test the fifth output\e[0m"
 
-#echo Installing the nginx
-#dnf install nginx -y &>> /tmp/logifile.log
+echo Installing the nginx
+dnf install nginx -y &>> /tmp/logifile.log
 
 yum list installed |grep -i nginx
 
-echo -e Removing the nginx
-dnf remove nginx -y &>> /tmp/logifile.log
+#echo -e Removing the nginx
+#dnf remove nginx -y &>> /tmp/logifile.log
+
+
+
+
+
 
 
 
