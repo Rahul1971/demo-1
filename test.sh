488cd370ef037bfa6e59eb387c95b173222f9ea0
Rahul1971 <rahulgandham1997@gmail.com>
2025-02-17 18:38:12 +0530

    Adding 1st line

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 echo This is to test the output
 echo -e "\e[31mThis is to test the second output\e[0m"
 echo -e "\e[32mThis is to test the third output\e[0m"
-echo -e "\e[33mThis is to test the fourth output\e[0m"  &>>/tmp/test.log
+echo -e "\e[33mThis is to test the fourth output\e[0m"  &>> /tmp/test.log
 echo -e "\e[34mThis is to test the fourth output\e[0m"
 echo -e "\e[35mThis is to test the fifth output\e[0m"
 
@@ -34,6 +34,7 @@ for i in {1..10}
 do
    echo "Number: $i"
 done
+
 
 
 
