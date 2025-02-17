9bd02323c792d15cfef18596602174449508d5df
Rahul1971 <rahulgandham1997@gmail.com>
2025-02-17 18:42:36 +0530

    Adding 1st line

--- test.sh
+++ test.sh
@@ -23,12 +23,14 @@ do
   i=$((i-1))
 done
 
-if [ $i -gt 11 ]
+
+if [ $i -eq 10 ]
 then
-echo " Number is greater than 10"
+echo " Number is equal than 10"
 else
   echo " Number is less than 10"
 fi
+
 
 for i in {1..10}
 do
