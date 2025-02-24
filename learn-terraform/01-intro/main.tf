685bc93b77b5fc5847f81a0a6fda73f4af625cfa
Rahul1971 <rahulgandham1997@gmail.com>
2025-02-24 20:25:40 +0530

    Adding 1st line

---
+++ learn-terraform/01-intro/main.tf
@@ -0,0 +1,34 @@
+provider "local" {}
+
+variable "x1" {
+  default = 10
+}
+
+output "x2" {
+  value = var.x1
+}
+
+#resource "local_file" "test3" {
+#  content = "This is to test the files in the dir"
+#  filename = "/tmp/testtt"
+#}
+
+variable "y9" {
+  default = [1,"king",false]
+}
+
+output "y5" {
+  value = var.y9[0]
+}
+output "y6" {
+  value = var.y9[1]
+}
+output "y7" {
+  value = var.y9[2]
+}
+
+
+variable "class" {}
+output "class" {
+  value = var.class
+}
